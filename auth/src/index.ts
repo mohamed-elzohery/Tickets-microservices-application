0d59da5de9e78003526c160847b0491ab5035c4f
mohamed-elzohery <m.zohery1998@gmail.com>
2022-05-05 09:31:05 +0200

    adding ingress service yaml

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+
 const express = require('express');
 
 const app = express();
 app.use(express.json());
+
+interface User {
+    userId: Number;
+    id: Number;
+    title: String;
+    body: String;
+}
+
+app.get('/api/users/currentuser', (req: Request, res: Response, next: NextFunction) => {
+    let user: User = {
+        userId: 1,
+        id: 1,
+        title: 'dvksdvsv',
+        body: "KOKO"
+    };
+    return res.json({user});
+})
 
 app.listen(5000, () => console.log('Auth service is running on port 5000'));
