bf6331630b386bae4cdcf912ea80d745cb993efb
mohamed-elzohery <m.zohery1998@gmail.com>
2022-05-05 08:06:14 +0200

    Initiate auth service on deployment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,4 +3,4 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.listen(5000, () => console.log('Auth service is running on port 5000'));
+app.listen(5000, () => console.log('Auth service is running on port 5000.'));
