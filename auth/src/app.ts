ba5ea628179754c9cd2e3cd5df3b0e5c2733d5c0
mohamed-elzohery <m.zohery1998@gmail.com>
2022-05-09 17:42:37 +0200

    Adding config for testing by jest

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,18 +1,19 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { authRouter } from "./routes/authRoutes";
 import {errorHandler} from './middlewares/app-error-handler'
-import {connect} from 'mongoose';
 import cookieParser from "cookie-parser";
+import { ErrorResponse } from './utils/ErrorResponse';
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-connect('mongodb://auth-mongo-srv:27017/users', () => {
-    console.log('connected to data base');
-});
+
 
 app.use('/api/users', authRouter);
+app.all('*', (req: Request, res: Response, next:NextFunction) => (
+    next(new ErrorResponse(404, 'Route not found', 'NotFound')))
+);
 app.use(errorHandler);
 
-app.listen(5000, () => console.log('Auth service is running on port 5000...'));
+export default app;
